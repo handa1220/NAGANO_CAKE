97c9f7b2adb34c9055caff0adff38d272778a027
handa1220 <108671890+handa1220@users.noreply.github.com>
2022-09-10 06:44:23 +0000

    [update]複数権限でのDivise導入

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,2 +1,6 @@
 class CartItem < ApplicationRecord
+  
+  belongs_to :cutomer
+  belongs_to :item
+  
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,7 @@
 class OrderDetail < ApplicationRecord
   enum production_status: { not_initiated: 0, awaiting_manufacture: 1,
     under_manufacture: 2, manufacturing_completed: 3 }
+    
+  belongs_to :order
+  
 end
