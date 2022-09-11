dc627c7172dcc27863b758ae8bb0e706a0387f98
handa1220 <108671890+handa1220@users.noreply.github.com>
2022-09-11 08:14:33 +0000

    [create]管理者のview及びcontrolleを作成、ルートを記述

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,15 @@ Rails.application.routes.draw do
     sessions: "admin/sessions"
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  namespace :admin do
+    resources :homes, only: [:top]
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :orders, only: [:show, :update]
+    resources :order_details, only: [:update]
+  end
+  
+  
 end
