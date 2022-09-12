10837d8a5e3e5d8f86edc3d35d862fed44e10431
handa1220 <108671890+handa1220@users.noreply.github.com>
2022-09-12 11:45:52 +0000

    [update]ルートの修正

--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,17 @@ Rails.application.routes.draw do
   
   resources :homes, only: [:top, :about]
   resources :items, only: [:index, :show]
-  resources :customers, only: [:show, :edit, :update, :unsubscribe, :withdraw]
-  resources :cart_items, only: [:index, :update, :destroy, :destroy_all, :create]
-  resources :orders, only: [:new, :confirm, :thanks, :create, :index, :show]
+  resources :cart_items, only: [:index, :update, :destroy, :create]
+  resources :orders, only: [:new, :create, :index, :show]
   resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+  get "customers/my_page" => "customers#show", as: "my_page_customers"
+  get "customers/information/edit" => "customers#edit", as: "edit_information_customers"
+  patch "customers/information" => "customers#update", as: "information_customers"
+  get "customers/unsubscribe", as: "unsubscribe_customers"
+  patch "customers/withdraw", as: "withdraw_customers"
+  post "orders/confirm", as: "confirm_orders"
+  get "orders/thanks", as: "thanks_orders"
+  delete "cart_items/destroy_all", as: "destroy_all"
   
   namespace :admin do
     resources :homes, only: [:top]
