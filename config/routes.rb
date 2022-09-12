4fb35464722b9d4578beca6a57ca106103c3e54c
handa1220 <108671890+handa1220@users.noreply.github.com>
2022-09-12 11:56:08 +0000

    [update]ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
+  root to: "homes#top"
+  get "about" => "homes#about", as: "about"
   resources :homes, only: [:top, :about]
   resources :items, only: [:index, :show]
   resources :cart_items, only: [:index, :update, :destroy, :create]
