bc2ca62a9f9860d2ce5988d8fce664561bccaa73
handa1220 <108671890+handa1220@users.noreply.github.com>
2022-09-20 13:34:11 +0000

    [create]会員情報編集機能の追加

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -26,7 +26,7 @@ class Admin::ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.new(params[:id])
+    @item = Item.find(params[:id])
     @item.update(item_params)
     redirect_to admin_item_path(@item.id)
   end
