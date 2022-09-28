f4d9863c5f51fc69186d57e281adc09e7a5f9d33
handa1220 <108671890+handa1220@users.noreply.github.com>
2022-09-28 13:18:39 +0000

    [create]管理者トップページの作成

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,8 +2,8 @@ class Order < ApplicationRecord
   enum payment_method: {credit_card: 0, transfer: 1 }
   enum order_status: {awaiting_payment: 0, payment_confirmation: 1,
     under_manufacture: 2, preparing_to_ship: 3, already_shipped: 4 }
-    
+
   belongs_to :customer
   has_many :order_details
-  
+
 end
