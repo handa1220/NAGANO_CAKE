277d889c4f8fd3bb3e354bd30570ac69dfde6f31
handa1220 <108671890+handa1220@users.noreply.github.com>
2022-10-10 06:50:18 +0000

    [patch]レイアウトの調整

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,9 +4,9 @@ class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :order_details
   belongs_to :genre
-  
+
   def get_image(width,height)
     image.variant(resize_to_limit: [width,height]).processed
   end
-  
+
 end
