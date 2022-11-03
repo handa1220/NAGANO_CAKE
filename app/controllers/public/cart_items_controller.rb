a5f989fbfe621cd43317c3718b7e4c679091411d
handa1220 <108671890+handa1220@users.noreply.github.com>
2022-11-03 06:46:35 +0000

    [create]商品詳細ページ作成

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,17 +1,29 @@
 class Public::CartItemsController < ApplicationController
   def index
   end
-  
+
   def update
   end
-  
+
   def destroy
   end
-  
+
   def destroy_all
   end
-  
+
   def create
+    cart_item = CartItem.new(cart_item_params)
+    item = Item.find(params[:id])
+    cart_item.item_id = item.id
+    cart_item.customer_id = current_customer.id
+    cart_item.save
+    redirect_to cart_items_path
   end
-  
+
+  protected
+
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id,:customer_id,:amount)
+  end
+
 end
