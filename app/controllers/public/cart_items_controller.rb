a7a3def34de45ce33c387fa505703eb205136189
handa1220 <108671890+handa1220@users.noreply.github.com>
2022-11-04 14:57:21 +0000

    [midway]カート機能作成途中

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -12,11 +12,7 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    cart_item = CartItem.new(cart_item_params)
-    item = Item.find(params[:id])
-    cart_item.item_id = item.id
-    cart_item.customer_id = current_customer.id
-    cart_item.save
+    
     redirect_to cart_items_path
   end
 
