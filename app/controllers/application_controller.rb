657c6306fdc4373d2ad150f63f10581c8449f74c
handa1220 <108671890+handa1220@users.noreply.github.com>
2022-11-15 14:06:18 +0000

    [midway]カート機能作成途中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number])
   end
 
+  def current_cart_item
+    if current_customer
+      current_cart = current_customer.cart || current_customer.create_cart!
+    else
+      current_cart =
+
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -3,4 +3,9 @@ class CartItem < ApplicationRecord
   belongs_to :cutomer
   belongs_to :item
   
+  
+  def price_including_tax
+    (item.price * 1.1) * amount
+  end
+  
 end
