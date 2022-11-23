acd4bd5925bbec51c08011bb929f561eca54ae93
handa1220 <108671890+handa1220@users.noreply.github.com>
2022-11-23 10:37:04 +0000

    [create]注文情報入力画面作成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,20 +1,21 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order = Order.new
   end
-  
+
   def confirm
   end
-  
+
   def thanks
   end
-  
+
   def create
   end
-  
+
   def index
   end
-  
+
   def show
   end
-  
+
 end
