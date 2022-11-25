400dd735a0200403cca098bb0dde7b549123c385
handa1220 <108671890+handa1220@users.noreply.github.com>
2022-11-25 15:31:23 +0000

    [midway]注文情報確認画面作成途中

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,16 +1,21 @@
 class Public::OrdersController < ApplicationController
+
   def new
     @order = Order.new
   end
 
   def confirm
+    @cart_item = CartItem.where(customer_id: current_customer.id)
+    @total = 0
+    @postage = 800
     @order = Order.new(order_params)
-    @address = Address.find(params[:order][:address_id])
     if params[:order][:select_address] == 0
       @order.postal_code = current_customer.postal_code
       @order.address = current_customer.address
       @order.name = current_customer.first_name + current_customer.last_name
-      
+    else if params[:order][:select_address] == 2
+      @address = Address.find(params[:order][:address_id])
+    end
   end
 
   def thanks
