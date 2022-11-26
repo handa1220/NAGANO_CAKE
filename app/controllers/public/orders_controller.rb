9f735d5f2454b09737080d2f990e5dcbedc5ea41
handa1220 <108671890+handa1220@users.noreply.github.com>
2022-11-26 02:06:19 +0000

    [midway]注文情報確認画面作成途中

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -9,11 +9,11 @@ class Public::OrdersController < ApplicationController
     @total = 0
     @postage = 800
     @order = Order.new(order_params)
-    if params[:order][:select_address] == 0
+    if params[:order][:select_address] == "0"
       @order.postal_code = current_customer.postal_code
       @order.address = current_customer.address
       @order.name = current_customer.first_name + current_customer.last_name
-    else if params[:order][:select_address] == 2
+    elsif params[:order][:select_address] == "2"
       @address = Address.find(params[:order][:address_id])
     end
   end
