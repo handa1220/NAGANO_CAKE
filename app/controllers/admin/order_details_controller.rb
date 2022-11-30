907e6d9de50edec881f5731f06bcf4d6f8bbb37c
handa1220 <108671890+handa1220@users.noreply.github.com>
2022-11-30 12:54:56 +0000

    [finish]ながのケーキECサイト作成完了

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,17 +1,25 @@
 class Admin::OrderDetailsController < ApplicationController
-  
+
   before_action :authenticate_admin!
-  
+
   def update
-    @order_details = OrderDetail.find(params[:id])
-    @order_details.update(order_detail_params)
-    redirect_to admin_order_path(@order_details.order_id)
+    order_detail = OrderDetail.find(params[:id])
+    order_detail.update(order_detail_params)
+    order_details = OrderDetail.where(order_id: order_detail.order_id).pluck('production_status')
+    if order_detail.production_status == "under_manufacture"
+      order = Order.find(order_detail.order_id)
+      order.update(order_status: 2)
+    elsif order_details.all? { |w| w == "manufacturing_completed" }
+      order = Order.find(order_detail.order_id)
+      order.update(order_status: 3)
+    end
+    redirect_to admin_order_path(order_detail.order_id)
   end
-  
+
   private
-  
+
   def order_detail_params
     params.require(:order_detail).permit(:production_status)
   end
-  
+
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -8,9 +8,15 @@ class Admin::OrdersController < ApplicationController
   end
 
   def update
-    @order = Order.find(params[:id])
-    @order.update(order_params)
-    redirect_to admin_order_path(@order.id)
+    order = Order.find(params[:id])
+    order.update(order_params)
+    if order.order_status == "payment_confirmation"
+      order_details = OrderDetail.where(order_id: order.id)
+      order_details.each do |order_detail|
+        order_detail.update(production_status: 1)
+      end
+    end
+    redirect_to admin_order_path(order.id)
   end
 
   private
